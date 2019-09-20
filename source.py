96c64317f615b3ab9a7f6df35f92944dfe15837a
J10c <49830650+J10c@users.noreply.github.com>
2019-09-20 19:45:17 +0800

    Update source.py

--- source.py
+++ source.py
@@ -1,7 +1,10 @@
 # from https://www.cnblogs.com/shuimohei/p/11345458.html
 # 目标：爬取豆瓣电影排行榜TOP250的电影信息
 # 信息包括：电影名字，上映时间，主演，评分，导演，一句话评价
-# 解析用学过的几种方法都实验一下①正则表达式.②BeautifulSoup③xpath
+# 解析用学过的几种方法都实验一下①正则表达式:
+#Beautiful
+#Soup
+#xpath
 import requests
 import re  # 正则表达式
 import json
